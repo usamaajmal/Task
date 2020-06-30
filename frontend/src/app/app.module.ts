7438ca3e0c7608e351c2e1801f2bc8acbe752c03
usamaajmal <usamaajmal@gmail.com>
2020-06-30 16:02:01 +0500

    As 4 hours completed, user login and registeration frontend pending, backend done, service done and coupons all 3 features completed.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { CouponComponent } from './coupon/coupon.component';
 import { NewCouponComponent } from './new-coupon/new-coupon.component';
 import { FormsModule, ReactiveFormsModule , FormGroup, FormControl, FormBuilder  } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CouponComponent,
-    NewCouponComponent
+    NewCouponComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
