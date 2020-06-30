089cc6ec804fc03ebb0691a5ef770da24efe39a1
usamaajmal <usamaajmal@gmail.com>
2020-06-30 13:49:32 +0500

    Coupan component and service generated

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CouponComponent } from './coupon/coupon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CouponComponent
   ],
   imports: [
     BrowserModule,
