440ee8947b4372c787d4d52a238bb1280d5340d1
usamaajmal <usamaajmal@gmail.com>
2020-06-30 12:38:31 +0500

    Adding New Coupan (Tested with postman)

--- routes/coupans.js
+++ routes/coupans.js
@@ -3,6 +3,27 @@ const router = express.Router();
 var stripe = require('stripe')('sk_test_51GzchSFIxbY9BazzQXAOJS3j5i0anefDZYFUHyNKeRi56NltrhWGkoqesjr6YoI0UtqLgVPg1vp0mVUEQqnRNpEI00GcrnxfMa');
 
 
+// add Coupan
+router.post('/add',(req,res)=>{
+    let obj = req.body;
+    stripe.coupons.create({
+        percent_off: obj.Coupan.percent,
+        duration: obj.Coupan.duration,
+        duration_in_months: obj.Coupan.months
+    }, (err,coupon)=>{
+        if (err) {
+            console.log("Error at \"Adding Coupon\"");
+            res.statusMessage = err;
+            res.status(401).end();
+        }
+        if (coupon){
+        console.log("\"Coupon\" added successfully");
+        res.send(coupon);
+        }
+    })
+	
+})
+
 // get all coupans
 router.get('/',(req,res)=>{
 	stripe.coupons.list((err,coupons)=>{
